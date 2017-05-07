6ff34016877521bb203a7009809c187e653d0d1e
justinilalonde <justinilalonde@gmail.com>
2017-05-06 21:31:54 -0400

    Basic Movement
    Ship can move. Crew pathing not moving with ship

---
+++ Assets/Destination.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destination : MonoBehaviour {
+    private GameObject destination;
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetButtonDown("move"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray))
+                Instantiate(destination, transform.position, transform.rotation);
+        }	
+	}
+}
